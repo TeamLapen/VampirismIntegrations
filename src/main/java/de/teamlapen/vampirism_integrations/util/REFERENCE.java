d7256f625b88ce2df89c753ac75845211765031b
maxanier <dev@maxgb.de>
2018-01-07 22:19:05 +0000

    Change required Vampirism version to make stuff work again for now. If old alpha versions of Vampirism are used it crashes, but . Closes TeamLapen/Vampirism#259

--- src/main/java/de/teamlapen/vampirism_integrations/util/REFERENCE.java
+++ src/main/java/de/teamlapen/vampirism_integrations/util/REFERENCE.java
@@ -6,7 +6,7 @@ public class REFERENCE {
     public static final String FORGE_VERSION = "@FVERSION@";
     public static final String MODID = "vampirism_integrations";
     public static final String FORGE_VERSION_MIN = "14.23.0.2550";
-    public static final String VAMPIRISM_VERSION_MIN = "1.4.0-alpha+11-21-22-25";
+    public static final String VAMPIRISM_VERSION_MIN = "1.4.0-alpha+01-07-19-32";
 
     public static final String VAMPIRISM_ID = "vampirism";
     public static final String DEPENDENCIES = "required-after:forge@[" + REFERENCE.FORGE_VERSION_MIN + ",);required-after:vampirism@[" + REFERENCE.VAMPIRISM_VERSION_MIN + ",];after:guideapi;after:mca@[1.12.2-5.3.1,);after:biomesoplenty;after:abyssalcraft";//TODO Vampirism Library
