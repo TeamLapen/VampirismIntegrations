d3e91942bb4e235737d67374d32e2c10862263a6
maxanier <dev@maxgb.de>
2020-10-19 22:55:41 +0200

    Finish 1.15

--- src/main/java/de/teamlapen/vampirism_integrations/EventHandler.java
+++ src/main/java/de/teamlapen/vampirism_integrations/EventHandler.java
@@ -2,9 +2,12 @@ package de.teamlapen.vampirism_integrations;
 
 import de.teamlapen.lib.lib.util.UtilLib;
 import de.teamlapen.lib.lib.util.VersionChecker;
+import de.teamlapen.vampirism.config.VampirismConfig;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModContainer;
@@ -25,31 +28,32 @@ public class EventHandler {
         VersionChecker.VersionInfo versionInfo = VampirismIntegrationsMod.instance.getVersionInfo();
         if (!versionInfo.isChecked())
             LOGGER.warn("Version check is not finished yet");
+
+        boolean isAdminLikePlayer = !ServerLifecycleHooks.getCurrentServer().isDedicatedServer() || UtilLib.isPlayerOp(event.getPlayer());
+
         if (!VampirismCompat.disableVersionCheck.get() && versionInfo.isNewVersionAvailable()) {
-            if (!ServerLifecycleHooks.getCurrentServer().isDedicatedServer() || UtilLib.isPlayerOp(event.getPlayer()) || event.getPlayer().getRNG().nextInt(5) == 0) {
-                if (event.getPlayer().getRNG().nextInt(4) == 0) {
-                    VersionChecker.Version newVersion = versionInfo.getNewVersion();
-                    //Inspired by @Vazikii's useful message
-                    event.getPlayer().sendMessage(new TranslationTextComponent("text.vampirism.outdated", versionInfo.getCurrentVersion().name, newVersion.name));
-                    String template = UtilLib.translate("text.vampirism.update_message");
-                    template = template.replaceAll("@download@", newVersion.getUrl() == null ? versionInfo.getHomePage() : newVersion.getUrl()).replaceAll("@forum@", versionInfo.getHomePage());
-                    ITextComponent component = ITextComponent.Serializer.fromJson(template);
-                    if (component != null) {
-                        event.getPlayer().sendMessage(component);
+
+                if (isAdminLikePlayer || event.getPlayer().getRNG().nextInt(5) == 0) {
+                    if (event.getPlayer().getRNG().nextInt(4) == 0) {
+                        VersionChecker.Version newVersion = versionInfo.getNewVersion();
+                        event.getPlayer().sendMessage(new TranslationTextComponent("text.vampirism.outdated", versionInfo.getCurrentVersion().name, newVersion.name));
+                        ITextComponent download = new TranslationTextComponent("text.vampirism.update_message.download").applyTextStyle(style -> style.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, newVersion.getUrl() == null ? versionInfo.getHomePage() : newVersion.getUrl())).setUnderlined(true).setColor(TextFormatting.BLUE));
+                        ITextComponent changelog = new TranslationTextComponent("text.vampirism.update_message.changelog").applyTextStyle(style -> style.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/vampirism changelog")).setUnderlined(true));
+                        ITextComponent modpage = new TranslationTextComponent("text.vampirism.update_message.modpage").applyTextStyle(style -> style.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, versionInfo.getHomePage())).setUnderlined(true).setColor(TextFormatting.BLUE));
+                        event.getPlayer().sendMessage(download.appendText(" ").appendSibling(changelog).appendText(" ").appendSibling(modpage));
                     }
                 }
-            }
+
 
         }
-        if (!ServerLifecycleHooks.getCurrentServer().isDedicatedServer() || UtilLib.isPlayerOp(event.getPlayer())) {
-            if (event.getPlayer().getRNG().nextInt(4) == 0) {
+            if (isAdminLikePlayer && event.getPlayer().getRNG().nextInt(4) == 0) {
                 List<IModCompat> list = VampirismIntegrationsMod.instance.compatLoader.getIncompatibleCompats();
                 for (IModCompat m : list) {
                     Optional<? extends ModContainer> mod = ModList.get().getModContainerById(m.getModID());
                     mod.ifPresent(modcontainer -> event.getPlayer().sendMessage(new StringTextComponent(String.format("Could not load Vampirism mod compat for %s because version %s is incompatible (Accepted %s)", m.getModID(), modcontainer.getModInfo().getVersion(), m.getAcceptedVersionRange()))));
                 }
             }
-        }
+
 
     }
 }
--- src/main/java/de/teamlapen/vampirism_integrations/bop/Biomes.java
+++ src/main/java/de/teamlapen/vampirism_integrations/bop/Biomes.java
@@ -2,11 +2,19 @@ package de.teamlapen.vampirism_integrations.bop;
 
 import biomesoplenty.api.biome.BOPBiomes;
 import de.teamlapen.vampirism.api.VampirismAPI;
+import net.minecraft.util.ResourceLocation;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 class Biomes {
+    private static final ResourceLocation hunter_camp_id = new ResourceLocation("vampirism", "hunter_camp");
     static void registerNoSundamageBiomes(BOPCompat compat) {
         if (compat.disabled_sundamage_ominous_woods.get() && BOPBiomes.ominous_woods.isPresent()) {
-            BOPBiomes.ominous_woods.ifPresent(b -> VampirismAPI.sundamageRegistry().addNoSundamageBiome(b.getRegistryName()));
+            BOPBiomes.ominous_woods.ifPresent(b -> {
+                VampirismAPI.sundamageRegistry().addNoSundamageBiome(b.getRegistryName());
+                VampirismAPI.worldGenRegistry().removeStructureFromBiomes(hunter_camp_id, Collections.singletonList(b));
+            } );
         }
     }
 }
