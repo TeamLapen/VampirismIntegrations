9ade0188c89a13925b653153245125624e43c52f
maxanier <dev@maxgb.de>
2020-11-14 17:58:38 +0100

    Update to 1.16.4 Fix BOP issue

--- src/main/java/de/teamlapen/vampirism_integrations/bop/Biomes.java
+++ src/main/java/de/teamlapen/vampirism_integrations/bop/Biomes.java
@@ -11,7 +11,7 @@ class Biomes {
     static void registerNoSundamageBiomes(BOPCompat compat) {
         if (compat.disabled_sundamage_ominous_woods.get()) {
             VampirismAPI.sundamageRegistry().addNoSundamageBiomes(BOPBiomes.ominous_woods.getLocation());
-            VampirismAPI.worldGenRegistry().removeStructureFromBiomes(hunter_camp_id, Arrays.asList(BOPBiomes.ominous_woods.getLocation(), BOPBiomes.withered_abyss.getLocation(), BOPBiomes.outback.getLocation()));
+            VampirismAPI.worldGenRegistry().removeStructureFromBiomes(hunter_camp_id, Arrays.asList(BOPBiomes.ominous_woods.getLocation(), BOPBiomes.withered_abyss.getLocation()));
         }
     }
 }
