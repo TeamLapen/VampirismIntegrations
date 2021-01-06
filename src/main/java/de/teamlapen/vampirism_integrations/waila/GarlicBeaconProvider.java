acae96cf627497939cdc40b8af82990238176910
maxanier <dev@maxgb.de>
2021-01-06 23:19:57 +0100

    Close #37

--- src/main/java/de/teamlapen/vampirism_integrations/waila/GarlicBeaconProvider.java
+++ src/main/java/de/teamlapen/vampirism_integrations/waila/GarlicBeaconProvider.java
@@ -4,10 +4,11 @@ import de.teamlapen.vampirism.tileentity.GarlicBeaconTileEntity;
 import mcp.mobius.waila.api.IComponentProvider;
 import mcp.mobius.waila.api.IDataAccessor;
 import mcp.mobius.waila.api.IPluginConfig;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 
 import java.util.List;
 
@@ -17,7 +18,7 @@ public class GarlicBeaconProvider implements IComponentProvider {
 
     @Override
     public void appendBody(List<ITextComponent> tooltip, IDataAccessor accessor, IPluginConfig config) {
-        accessor.getStack().getItem().addInformation(accessor.getStack(), accessor.getWorld(), tooltip, ITooltipFlag.TooltipFlags.NORMAL);
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> () -> ClientUtil.gatherTooltips(accessor.getStack(), accessor.getWorld(), tooltip));
         TileEntity t = accessor.getTileEntity();
         if (t instanceof GarlicBeaconTileEntity) {
             int fueled = ((GarlicBeaconTileEntity) t).getFuelTime();
