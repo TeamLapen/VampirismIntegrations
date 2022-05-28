163d79a88b743ab21c086b95c6ab97168ffd12d9
maxanier <dev@maxgb.de>
2022-05-28 13:14:23 +0200

    Some fixes

--- src/main/java/de/teamlapen/vampirism_integrations/bop/Biomes.java
+++ src/main/java/de/teamlapen/vampirism_integrations/bop/Biomes.java
@@ -1,12 +1,12 @@
 package de.teamlapen.vampirism_integrations.bop;
 
+import biomesoplenty.api.biome.BOPBiomes;
 import de.teamlapen.vampirism.api.VampirismAPI;
-import net.minecraft.resources.ResourceLocation;
 
 class Biomes {
     static void registerNoSundamageBiomes(BOPCompat compat) {
         if (compat.disabled_sundamage_ominous_woods.get()) {
-            VampirismAPI.sundamageRegistry().addNoSundamageBiomes(new ResourceLocation("biomesoplenty:ominous_woods"));
+            VampirismAPI.sundamageRegistry().addNoSundamageBiomes(BOPBiomes.OMINOUS_WOODS.getRegistryName());
         }
     }
 }
