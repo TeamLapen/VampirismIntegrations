ea9829c3868fd95047e694c55dbd93f4e0021ba9
maxanier <dev@maxgb.de>
2026-01-21 12:17:41 +0100

    Fix MCA schedule

--- src/main/java/de/teamlapen/vampirism_integrations/mca/ConvertedVillagerEntityMCA.java
+++ src/main/java/de/teamlapen/vampirism_integrations/mca/ConvertedVillagerEntityMCA.java
@@ -253,9 +253,9 @@ public class ConvertedVillagerEntityMCA extends VillagerEntityMCA implements Cur
     public void registerBrainGoals(@Nonnull Brain<VillagerEntityMCA> brain) {
         VillagerTasksMCA.initializeTasks(this, brain);
         AgeState age = AgeState.byCurrentAge(this.getAge());
-        if (age != AgeState.ADULT) {
+        if (age == AgeState.ADULT) {
             brain.setSchedule(ModVillage.CONVERTED_DEFAULT.get());
-            brain.updateActivityFromSchedule(this.level().getDayTime(), this.level().getGameTime() + 21 /*Trick update*/);
+            brain.updateActivityFromSchedule(this.level().getDayTime(), this.level().getGameTime());
         }
     }
 
