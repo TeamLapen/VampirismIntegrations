40afc517ea7affb260799811278639994bf15ca0
maxanier <dev@maxgb.de>
2026-01-21 12:36:18 +0100

    Bump version and dependencies

--- src/main/java/de/teamlapen/vampirism_integrations/jade/provider/ResearchTableProvider.java
+++ src/main/java/de/teamlapen/vampirism_integrations/jade/provider/ResearchTableProvider.java
@@ -1,11 +1,8 @@
 package de.teamlapen.vampirism_integrations.jade.provider;
 
-import de.teamlapen.vampirism.blocks.HunterTableBlock;
-import de.teamlapen.vampirism.entity.player.hunter.HunterLevelingConf;
 import de.teamlapen.vampirism_integrations.jade.JadePlugin;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.state.BlockState;
 import snownee.jade.api.BlockAccessor;
 import snownee.jade.api.IBlockComponentProvider;
 import snownee.jade.api.ITooltip;
